4619e142608e0283765736ac34d0de1f1df4a6dd
MScZeFa <109414164+MScZeFa@users.noreply.github.com>
2025-07-14 14:47:55 +0200

    Update InvokeScript_Install.ps1
    Pfadanpassungen

--- 4_deployment/InvokeScript_Install.ps1
+++ 4_deployment/InvokeScript_Install.ps1
@@ -3,9 +3,9 @@
 # Invoking ArcGIS Configuration Script
 # Set Path to easily access logs (the location you run the Invoke from will be where the Logs show up)
 
-Set-Location -Path "E:\Logs"
+Set-Location -Path "C:\Automation\Logs"
 
-Invoke-ArcGISConfiguration -ConfigurationParametersFile "D:\EsriInstall\DSC\BaseEnterpriseDeployment.json" -Mode InstallLicenseConfigure -DebugSwitch
+Invoke-ArcGISConfiguration -ConfigurationParametersFile "C:\Automation\DSC\basic.json" -Mode InstallLicenseConfigure -DebugSwitch
 
 # if successful, run without -DebugSwitch to turn off Debug Mode on ArcGIS Components
 # Invoke-ArcGISConfiguration -ConfigurationParametersFile "D:\EsriInstall\DSC\BaseEnterpriseDeployment.json" -Mode InstallLicenseConfigure
